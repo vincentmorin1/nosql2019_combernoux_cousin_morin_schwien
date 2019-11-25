4b1c1aa7a336a89bf827303ae2736dcc1f14ce38
benjpresent <benjamin.cousin@telecomnancy.net>
2019-11-25 22:31:33 +0100

    Parsing

---
+++ src/main/java/mongodb/parsingSwiss.java
@@ -0,0 +1,21 @@
+package mongodb;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class parsingSwiss {
+
+    public static void main (String[] args) {
+        try (Scanner scanner = new Scanner(new File("/Users/benjamincousin/Documents/NoSQL/project/DataBases/VersionSWISS.tab"));) {
+            int nword = 0;
+            while (scanner.hasNextLine() && nword<15) {
+                String sent = scanner.nextLine();
+                nword++;
+                System.out.printf("%3d) %s%n", nword, sent);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
