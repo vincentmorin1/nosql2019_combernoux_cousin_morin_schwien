b74c4d0bed0e0037b3d0c66d46e0cdd6b18a391b
Vincent Morin <vincent.morin@telecomnancy.eu>
2019-11-25 22:43:33 +0100

    maj

--- src/main/java/mongodb/mongodbConnection.java
+++ src/main/java/mongodb/mongodbConnection.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Indexes;
 import org.bson.Document;
 
 import java.util.Arrays;
@@ -15,7 +16,8 @@ public class mongodbConnection {
         try {
             MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
             MongoDatabase database = mongoClient.getDatabase("mydb");
-            MongoCollection<Document> collection = database.getCollection("test");
+            MongoCollection<Document> collectionReviewedProtein = database.getCollection("reviewedProtein");
+            MongoCollection<Document> collectionUnreviewedProtein = database.getCollection("unreviewedProtein");
 
             Document doc = new Document("name", "MongoDB")
                     .append("type", "database")
@@ -23,13 +25,18 @@ public class mongodbConnection {
                     .append("versions", Arrays.asList("v3.2", "v3.0", "v2.6"))
                     .append("info", new Document("x", 203).append("y", 102));
 
-            collection.insertOne(doc);
+            collectionReviewedProtein.insertOne(doc);
 
-            Document myDoc = collection.find().first();
+            Document myDoc = collectionReviewedProtein.find().first();
             System.out.println(myDoc.toJson());
+
+            collectionReviewedProtein.createIndex(Indexes.text("description"));
+            collectionReviewedProtein.createIndex(Indexes.text("name"));
+            collectionReviewedProtein.createIndex(Indexes.text("id"));
 
         } catch (MongoException mongoExObj){
             mongoExObj.printStackTrace();
         }
     }
+
 }
--- src/main/java/mongodb/parsingSwiss.java
+++ src/main/java/mongodb/parsingSwiss.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class parsingSwiss {
 
     public static void main (String[] args) {
-        try (Scanner scanner = new Scanner(new File("/Users/benjamincousin/Documents/NoSQL/project/DataBases/VersionSWISS.tab"));) {
+        try (Scanner scanner = new Scanner(new File("./VersionSWISS.tab"));) {
             int nword = 0;
             while (scanner.hasNextLine() && nword<15) {
                 String sent = scanner.nextLine();
