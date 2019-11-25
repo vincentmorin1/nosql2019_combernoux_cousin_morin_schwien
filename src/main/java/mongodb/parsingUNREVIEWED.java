0db13d3f508847c8a8cc99b25f6f2ae0968a1920
Vincent Morin <vincent.morin@telecomnancy.eu>
2019-11-25 22:57:23 +0100

    ajout parsing unreviewed

---
+++ src/main/java/mongodb/parsingUNREVIEWED.java
@@ -0,0 +1,21 @@
+package mongodb;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class parsingUNREVIEWED {
+
+    public static void main (String[] args) {
+        try (Scanner scanner = new Scanner(new File("./VersionUNREVIEWED.tab"));) {
+            int nword = 0;
+            while (scanner.hasNextLine() && nword<15) {
+                String sent = scanner.nextLine();
+                nword++;
+                System.out.printf("%3d) %s%n", nword, sent);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
